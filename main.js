b90ed6c6ddd99956167b35210a91f66a3eaea5d3
Calacly <54817827+Calacly@users.noreply.github.com>
2020-09-04 15:03:27 +0200

    Make zip-line slide responsive

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const leftTree = document.querySelector('#left-tree');
 const rightTree = document.querySelector('#right-tree');
 const woman = document.querySelector('#woman');
-const hero = document.querySelector('#hero');
-const angeboteSection = document.querySelector('#angeboteSection');
+// const hero = document.querySelector('#hero');
+// const angeboteSection = document.querySelector('#angeboteSection');
 
 window.addEventListener("scroll", () => {
     let scrolled = window.pageYOffset;
-    let intViewportWidth = window.innerWidth;
     console.log(scrolled)
     
     leftTree.style.left = `-${scrolled * .7}px`;
